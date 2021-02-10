05816242734342a64ae2f493a5d4149007edaef1
Rafaela <barrenarafaela@gmail.com>
2021-02-10 20:30:09 +0100

    En la clase coche añado el atributo kilometros

--- ActividadUF3_2/src/ejer2/pojo/Coche.java
+++ ActividadUF3_2/src/ejer2/pojo/Coche.java
@@ -7,15 +7,17 @@ public class Coche {
 	private String matricula;
 	private String modelo;
 	private String marca;
+	private int kilometros;
 	
 	
 	
-	public Coche(int id, String matricula, String modelo, String marca) {
+	public Coche(int id, String matricula, String modelo, String marca,int kilometros) {
 	
 		this.id = id;
 		this.matricula = matricula;
 		this.modelo = modelo;
 		this.marca = marca;
+		this.kilometros = kilometros;
 	}
 
 
@@ -64,6 +66,18 @@ public class Coche {
 
 	public void setMarca(String marca) {
 		this.marca = marca;
+	}
+
+
+
+	public int getKilometros() {
+		return kilometros;
+	}
+
+
+
+	public void setKilometros(int kilometros) {
+		this.kilometros = kilometros;
 	}
 	
 	
